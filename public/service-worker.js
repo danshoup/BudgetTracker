9ea6ca3eb0d2d7484dd4089cb2f25f1b83cc621e
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-08-14 16:22:35 -0400

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Uncomment the lines below
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
@@ -18,7 +18,7 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", function (evt) {
     // pre cache image data
     evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/data"))
       );
       
     // pre cache all static assets
@@ -51,20 +51,6 @@ self.addEventListener("install", function (evt) {
   });
   
 
-//   self.addEventListener('fetch', function(evt) {
-//     // code to handle requests goes here
-//     if(evt.request.url.includes('api')
-//     });
-
-//     evt.respondWith(
-//         caches.open(CACHE_NAME).then(cache => {
-//             return cache.match(evt.request).then(response => {
-//             return response || fetch(evt.request);
-//             });
-//         })
-//         );
-
-
   // fetch
   self.addEventListener("fetch", function(evt) {
     // cache successful requests to the API
@@ -89,7 +75,7 @@ self.addEventListener("install", function (evt) {
       );
   
       return;
-    }
+    };
 
     evt.respondWith(
         caches.open(CACHE_NAME).then(cache => {
@@ -99,11 +85,4 @@ self.addEventListener("install", function (evt) {
         })
     );
 
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-//     evt.respondWith(
-//       caches.match(evt.request).then(function(response) {
-//         return response || fetch(evt.request);
-//       })
-//     );
-//   });
+  })
